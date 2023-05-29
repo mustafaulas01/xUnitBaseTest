f4715a4b54091d6fa99c7d8e74f200e18c13b525
mustafaulasdogostore <mustafa.ulas@dogostore.com>
2023-05-29 11:52:25 +0300

    New Asserts Methods have been Added
    --Comparing Type Assert Method
    --Checking Regex Control Assert Method
    -- Controlling Bool value Assert Method

--- XUnitTest.Test/CalculatorTest.cs
+++ XUnitTest.Test/CalculatorTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NuGet.Frameworks;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,6 +19,7 @@ namespace XUnitTest.Test
             //Arrange ,Act ,Assert
 
             //Nesne örneği yada değişkenleri initialize edeceğim yer burasıdır.
+            //to create object instance or this is the place that initiliaze values
             //Arrange
             int a = 5;
             int b = 20;
@@ -26,18 +28,27 @@ namespace XUnitTest.Test
             //Arrange
 
             //Test edeceğimiz metotları çalıştıracağımız yerlerdir.
+             //This is the place which we are going to test out methods
             //Act 
             var total=calculator.Add(a, b);
 
             //Act
 
             //Act evresinden çıkan sonucun benim beklediğim sonuç mu değil mi,doğrulama evresi
+            //Checking expecting result and real returning result
+
             //Assert
             var names = new List<string>() { "Fatih", "Emre", "Hatice" };
 
             //Assert.Equal<int>(25, total);
             //Assert.Contains("Mustafa", "Mustafa Ulas");
-              Assert.Contains(names, a => a == "Emre");
+            //Assert.Contains(names, a => a == "Emre");
+            //checking  and comparing types
+            //Assert.True("Ahmet".GetType() == typeof(string));
+
+            //begins with "car" regex control
+            var regex = "^car";
+            Assert.Matches(regex, "car is red");
             //Assert
 
         }
